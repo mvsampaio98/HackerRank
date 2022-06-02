9d58beca08e974748f196c01d5dcb1e79b40210c
Marcelo Sampaio <78426140+mvsampaio98@users.noreply.github.com>
2022-06-02 20:30:06 -0300

    Add files via upload

---
+++ Even_Odd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+n = int(input('Digite um Numero: '))
+if (n%2 == 0):
+    print('{} is even'.format(n))
+else:
+    print('{} is odd'.format(n))
+
