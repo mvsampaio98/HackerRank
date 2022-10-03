3387f468c08a7e9c11c63055814461d34e1b8f13
mvsampaio98 <mvsampaio98@gmail.com>
2022-10-03 20:36:54 -0300

    Lists

---
+++ Python/Basic_Data_Types/Lists.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    N = int(input())
+    lista = []
+    for i in range(N):
+        comando = input().split()
+        if comando[0] == 'insert':
+            lista.insert(int(comando[1]),int(comando[2]))
+        elif comando[0] == 'print':
+            print(lista)
+        elif comando[0] == 'remove':
+            lista.remove(int(comando[1]))
+        elif comando[0] == 'append':
+            lista.append(int(comando[1]))
+        elif comando[0] == 'sort':
+            lista.sort()
+        elif comando[0] == 'pop':
+            lista.pop()
+        elif comando[0] == 'reverse':
+            lista.reverse()
